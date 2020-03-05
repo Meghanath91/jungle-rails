7ced0cf597894824e17ada5bf2408d0eee83590a
Meghanath91 <meghanath.balaji@gmail.com>
2020-03-05 18:21:28 -0500

    About us feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resource :about, only: [:show]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -16,6 +17,8 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
