8c5fd7af67d69c512934fb372dd6a775faccf0eb
Meghanath91 <meghanath.balaji@gmail.com>
2020-03-05 15:03:58 -0500

    admin dashboard page updated

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
   def show
+    @number_of_products =Product.count
+    @number_of_categories = Category.count
   end
 end
