53f96de14f1a9af3a2232989de8aba7c4a976229
Meghanath91 <meghanath.balaji@gmail.com>
2020-03-05 22:12:47 -0500

    admin categories completed

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,30 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['username'], password: ENV['password']
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'New category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name 
+    )
+  end
+
+end
