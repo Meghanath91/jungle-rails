0a943b14df7757c208427a802c570a19b7067e18
Meghanath91 <meghanath.balaji@gmail.com>
2020-03-10 13:39:24 -0400

    product details tested

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,39 @@
+
+
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see product details when click on product" do
+    # ACT
+    visit root_path
+
+    page.first( 'article.product header a').click
+
+    visit 'products/1'
+
+
+    
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    # expect(page).to have_css 'article.product', count: 1
+  end
+end
